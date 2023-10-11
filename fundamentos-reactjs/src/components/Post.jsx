4297a85d66347a9c778ce3a00b47ad78e39bb008
Sthefany Carneiro <sthef.carneiro@gmail.com>
2023-10-11 20:51:30 -0300

    resolvendo importação do post

--- fundamentos-reactjs/src/components/Post.jsx
+++ fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,4 @@
-import styles from './Post.modules.css';
+import styles from './Post.module.css';
 
 export function Post() {
     return (
@@ -12,10 +12,14 @@ export function Post() {
                     </div>
                 </div> 
 
-                <time dateTime=''>
-                    Publicado há 1h
-                </time>
+                <time title="11 de Outubro às 20:17" dateTime='2023-10-11 20:17:30'>Publicado há 1h</time>
             </header>
+            <div className={styles.content}> 
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+            <p>👉 <a href="">jane.design/doctorcare</a></p>
+            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
         </article>
     )
 }
