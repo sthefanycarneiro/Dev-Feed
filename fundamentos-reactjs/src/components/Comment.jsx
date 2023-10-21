9e51926d0f16089e4950172915220a267ea52659
Sthefany Carneiro <sthef.carneiro@gmail.com>
2023-10-20 21:15:56 -0300

    add component comment

---
+++ fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import  styles from './Comment.module.css'
+import { PiTrashDuotone , PiThumbsUpDuotone } from "react-icons/pi";
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://images.unsplash.com/photo-1580894908361-967195033215?auto=format&fit=crop&q=80&w=300&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+
+                        <div className={styles.authorAndTime}>
+                            <strong>Maria Helena</strong>
+                            <time title="11 de Outubro às 20:17" dateTime='2023-10-11 20:17:30'>Cerca de 2h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <PiTrashDuotone size={20}/>
+                        </button>
+                    </header>
+
+                    <p> Muito bom, Daniel! parabéns!! </p>
+                </div>
+
+                <footer>
+                    <button>
+                        <PiThumbsUpDuotone/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- fundamentos-reactjs/src/components/Post.jsx
+++ fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +6,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"/>
+                    <img className={styles.avatar} src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=50"/>
                     <div className={styles.authorInfo}> 
                         <strong>Daniel Gallo</strong>
                         <span>Web Developer</span>
@@ -15,10 +16,31 @@ export function Post() {
                 <time title="11 de Outubro às 20:17" dateTime='2023-10-11 20:17:30'>Publicado há 1h</time>
             </header>
             <div className={styles.content}> 
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
-            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '} <a href="">dgallo.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto{' '}</a>
+                    <a href="">#nlw{' '}</a>
+                    <a href="">#rocketseat{' '}</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                
+                <textarea
+                placeholder="Deixe um comentário"
+                />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
